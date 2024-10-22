907c252a186c51d78d4b6ea63460935f88125df7
Faiza <faraz_ul_islam@yahoo.com>
2024-10-22 12:40:53 +0100

    work in progress

--- Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#/Program.cs
+++ Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Use the string's IndexOf() and Substring() helper methods
+
+
+//Write code to find parenthesis pairs embedded in a string
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf("(");
+int closingPosition = message.IndexOf(")");
+
+Console.WriteLine($"{openingPosition} \n {closingPosition}");
+
+// Add code to retrieve the value between parenthesis
+string msg = "Find what is (inside the parentheses)";
+
+int openingPos = msg.IndexOf('(');
+int closingPos = msg.IndexOf(')');
+
+//int length = closingPos - openingPos;
+//Console.WriteLine(message.Substring(openingPos, length));
+
+//Modify the starting position of the sub string
+openingPos = openingPos + 1;
+
+int length = closingPos - openingPos;
+Console.WriteLine(message.Substring(openingPos, length));
+
+///////////////
+
+string msage = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openPosition = msage.IndexOf("<span>");
+int closPosition = msage.IndexOf("</span>");
+
+//openPosition += 6;
+openPosition += openSpan.Length;
+int lngth = closPosition - openPosition;
+Console.WriteLine(msage.Substring(openPosition, lngth));
+
+// Use the string's IndexOf() and LastIndexOf() helper methods
+
+string messageVal = "hello there!";
+
+int first_h = messageVal.IndexOf('h');
+int last_h = messageVal.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{messageVal}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
+Console.WriteLine("-----------------------------");
+
+string val = "Big Dog";
+val = val.Replace("B", "D");
+Console.WriteLine(val);
--- Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#/obj/Debug/net8.0/Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#.AssemblyInfo.cs
+++ Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#/obj/Debug/net8.0/Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea709fd39af88c15788e41990bed959ec3f9ddc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62050aa96bab5118eb82c7aa9646f7a6ce91d53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modify-the-content-of-strings-using-built-in-string-data-type-methods-in-C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
